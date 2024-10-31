d40a4f94c416e81856d5bafa16c978ea99783d03
albertboo9 <booalbert60@gmail.com>
2024-10-31 12:07:32 +0100

    modif20

--- machine_learning/server/server.py
+++ machine_learning/server/server.py
@@ -1,22 +1,32 @@
+
 import paho.mqtt.client as mqtt
+import joblib
+
+model = joblib.load('../models/regressionLineaire.joblib')
+
+# Paramètres de connexion
+broker_address = "127.0.0.1"
+port = 8888
+user = "python_server"
+password = "mypassword"
+
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connecté au broker MQTT avec succès !")
-        client.subscribe("home/fire_detection")  # S'abonner au topic
+        client.publish("home/fire_detection", "hello mqtt!")
+        client.subscribe("home/fire_detection")
     else:
-        print("Échec de la connexion (code de retour " + str(rc) + ")")
+        print(f"Échec de la connexion (code de retour {str(rc)})")
+
 
 def on_message(client, userdata, msg):
-    print(f"Message reçu sur le topic {msg.topic}: {str(msg.payload.decode('utf-8'))}")  # Décoder le payload en UTF-8
-
-broker_address = "votre_adresse_ip"
-port = 8888
+     print(msg.topic + " " + str(msg.payload))
 
 client = mqtt.Client()
+client.username_pw_set(user, password)
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(broker_address, port, 60)
-
+client.connect(broker_address, port)
 client.loop_forever()
