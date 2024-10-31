646f8d665efbc34c916f294120431f90c7c21fe9
albertboo9 <booalbert60@gmail.com>
2024-10-31 13:30:31 +0100

    modif23

--- machine_learning/server/server.py
+++ machine_learning/server/server.py
@@ -1,8 +1,14 @@
 
 import paho.mqtt.client as mqtt
 import joblib
+import json
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
+# désérialisation des composantes necessaire pour notre travail
 model = joblib.load('../models/regressionLineaire.joblib')
+#apc = joblib.load('../models/APC.joblib')
+#scaler = joblib.load('../models/scaler.joblib')
 
 # Paramètres de connexion
 broker_address = "127.0.0.1"
@@ -10,17 +16,19 @@ port = 8888
 user = "python_server"
 password = "mypassword"
 
+def on_message(client, userdata, msg):
+     print(msg.topic + " " + str(msg.payload))
+     data = json.load(msg.payload)
 
-def on_connect(client, userdata, flags, rc):
+     dataScaled = scaler.fit_tranform(data)
+
+
+def on_connect(client, userdata, flags, rc):.
     if rc == 0:
         print("Connecté au broker MQTT avec succès !")
         client.subscribe("home/fire_detection")
     else:
         print(f"Échec de la connexion (code de retour {str(rc)})")
-
-
-def on_message(client, userdata, msg):
-     print(msg.topic + " " + str(msg.payload))
 
 client = mqtt.Client()
 client.username_pw_set(user, password)
